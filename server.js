c9b19278e9d47817d0a09a6312ca8302fb6415fe
unknown <unnati.mistry09@gmail.com>
2022-01-26 16:12:52 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+const dburl='mongodb+srv://unnati:unnati@cluster0.1vswr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(dburl, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+.then(()=>{ console.log("connected successfully")})
+.catch(()=>{ console.log(" No connection")})
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000,()=>{
+  console.log("server started")
+})
